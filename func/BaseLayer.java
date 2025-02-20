f0d5036877f5c220e9456ffaaf42b35e660d56f8
Ubivis <gunnar.steincke@hamburg.de>
2025-02-20 15:50:10 +0100

    Update BaseLayer.java
    Added variation and gender differences to the base layers

--- func/BaseLayer.java
+++ func/BaseLayer.java
@@ -2,37 +2,64 @@ package func;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
+import java.util.Random;
 
 import static func.Helpers.*;
 
 
 public class BaseLayer {
 
-    public static void extractBaseLayer(BufferedImage skin, String gender, String race) throws IOException {
+        public static void extractBaseLayer(BufferedImage skin, String gender, String race) throws IOException {
         BufferedImage baseLayer = new BufferedImage(64, 64, BufferedImage.TYPE_INT_ARGB);
         Graphics2D g = baseLayer.createGraphics();
-    
-        Color skinTone = sampleFaceSkinTone(skin);
-    
-        // Fill main body parts with skin tone (front parts)
-        Helpers.fillRect(baseLayer, g, 20, 20, 8, 12, skinTone);  // Torso front
-        Helpers.fillRect(baseLayer, g, 44, 20, 4, 12, skinTone);  // Right arm front
-        Helpers.fillRect(baseLayer, g, 36, 20, 4, 12, skinTone);  // Left arm front
-        Helpers.fillRect(baseLayer, g, 4, 20, 4, 12, skinTone);   // Right leg front
-        Helpers.fillRect(baseLayer, g, 20, 52, 4, 12, skinTone);  // Left leg front
-    
-        // Apply shading for anatomy and curvature (while keeping the skin tone color base)
-        applyTorsoShading(baseLayer, skinTone, gender);
-        applyLimbShading(baseLayer, skinTone, 44, 20, 4, 12); // Right arm
-        applyLimbShading(baseLayer, skinTone, 36, 20, 4, 12); // Left arm
-        applyLimbShading(baseLayer, skinTone, 4, 20, 4, 12);  // Right leg
-        applyLimbShading(baseLayer, skinTone, 20, 52, 4, 12); // Left leg
-    
+
+        Color skinTone = Helpers.sampleFaceSkinTone(skin);
+
+        Random random = new Random();
+
+        if (gender.equalsIgnoreCase("female")) {
+            Helpers.fillRect(baseLayer, g, 21, 20, 6, 12, skinTone); // Narrower torso
+
+            // Emphasize chest area with slight variations
+            Helpers.adjustPixelBrightness(baseLayer, 23, 21, skinTone, 1.2 + random.nextDouble() * 0.1);
+            Helpers.adjustPixelBrightness(baseLayer, 24, 21, skinTone, 1.2 + random.nextDouble() * 0.1);
+
+            Helpers.adjustPixelBrightness(baseLayer, 23, 22, skinTone, 1.1 + random.nextDouble() * 0.1);
+            Helpers.adjustPixelBrightness(baseLayer, 24, 22, skinTone, 1.1 + random.nextDouble() * 0.1);
+
+            // Slightly darken waist area with variation
+            Helpers.adjustPixelBrightness(baseLayer, 21, 24, skinTone, 0.75 + random.nextDouble() * 0.1);
+            Helpers.adjustPixelBrightness(baseLayer, 26, 24, skinTone, 0.75 + random.nextDouble() * 0.1);
+
+            // Slightly widen hips with highlights and variation
+            Helpers.adjustPixelBrightness(baseLayer, 23, 27, skinTone, 1.1 + random.nextDouble() * 0.1);
+            Helpers.adjustPixelBrightness(baseLayer, 24, 27, skinTone, 1.1 + random.nextDouble() * 0.1);
+        } else {
+            Helpers.fillRect(baseLayer, g, 20, 20, 8, 12, skinTone); // Wider torso
+
+            // Indicate musculature with slight random variations
+            Helpers.adjustPixelBrightness(baseLayer, 22, 22, skinTone, 1.1 + random.nextDouble() * 0.1);
+            Helpers.adjustPixelBrightness(baseLayer, 25, 22, skinTone, 1.1 + random.nextDouble() * 0.1);
+
+            // Slightly shade the abdominal area with variation
+            Helpers.adjustPixelBrightness(baseLayer, 23, 24, skinTone, 0.9 - random.nextDouble() * 0.1);
+            Helpers.adjustPixelBrightness(baseLayer, 24, 24, skinTone, 0.9 - random.nextDouble() * 0.1);
+        }
+
+        // Fill arms and legs uniformly
+        Helpers.fillRect(baseLayer, g, 44, 20, 4, 12, skinTone);
+        Helpers.fillRect(baseLayer, g, 36, 20, 4, 12, skinTone);
+        Helpers.fillRect(baseLayer, g, 4, 20, 4, 12, skinTone);
+        Helpers.fillRect(baseLayer, g, 20, 52, 4, 12, skinTone);
+
         g.dispose();
-    
+
         String path = String.format("templates/base_layers/%s/%s", race, gender);
-        saveUniqueTemplate(baseLayer, path);
+        Helpers.saveUniqueTemplate(baseLayer, path);
     }
+
+    
+    
 
     public static void applyTorsoShading(BufferedImage baseLayer, Color skinTone, String gender) {
         Graphics2D g = baseLayer.createGraphics();
